7f26dd147ddf973ff7ff53539f30eb1b6634241d
Yasir ÇELİK <yasirc33@hotmail.com>
2017-05-17 00:06:53 +0300

    Create asalsayi.py
    python3x dilinde

---
+++ asalsayi.py
@@ -0,0 +1,19 @@
+deger = input("Kaça kadar ki asal sayıları arıyorsunuz? : ")
+asal = []
+
+for i in range(2, int(deger)):
+    flag = False
+    for j in range(2, i):
+        if i % j == 0:
+            flag = True
+            break
+    if not flag:
+        asal.append(i)
+
+for i in asal:
+    print (i)
+
+print ("0 -",deger," arasında toplam",len(asal)," tane asal sayı vardır.")
+
+
+@ysrclk
